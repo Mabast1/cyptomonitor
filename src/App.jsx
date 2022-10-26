1aa51ef7d5182a0d5be2b2190115afcc157a5c13
Mabast <mabast.niga@yahoo.com>
2022-10-26 14:37:31 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar">Navbar</div>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="main">
         Layout
         <div className="routes">
